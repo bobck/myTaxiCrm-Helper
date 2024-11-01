18812a7cadb5ca48c8428a06a21309c5c10c5447
bobozlo <bobckx10@gmail.com>
2024-11-01 01:30:04 +0100

    Move working drivers by INDEFINITE_LEAVE

--- src/sql/working_drivers_by_id.sql
+++ src/sql/working_drivers_by_id.sql
@@ -4,5 +4,5 @@ FROM
 	drivers d
 WHERE
 	d.company_id = '4ea03592-9278-4ede-adf8-f7345a856893'
-	AND d.inner_status = 'WORKING'
+	AND d.inner_status in ('INDEFINITE_LEAVE','WORKING')
 	AND d.id = ANY($1)
