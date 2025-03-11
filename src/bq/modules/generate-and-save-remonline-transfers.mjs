d9dae39f879a4ca29be03a91ccf544d8bc1e1ff7
iwouldneverto <glebsmile3303@gmail.com>
2025-03-11 15:03:02 +0100

    response mapping done

--- src/bq/modules/generate-and-save-remonline-transfers.mjs
+++ src/bq/modules/generate-and-save-remonline-transfers.mjs
@@ -1,5 +1,27 @@
 import { getLocations, getTransfers } from "../../remonline/remonline.utils.mjs";
 import { remonlineTokenToEnv } from "../../remonline/remonline.api.mjs";
+const splitTransfers=({_transfers})=>_transfers.reduce((acc, transfer)=>{
+    const _transfer=structuredClone(transfer);
+    const _products=structuredClone(_transfer.products).map(product=>{
+        const _product={...product,uom_id:product.uom.id,transfer_id:_transfer.id }
+        if(!acc.uoms.some((uom)=>uom.id===product.uom.id)){
+            acc.uoms.push(_product.uom);
+        }
+        delete _product.uom;
+        return _product;
+    });
+
+    delete _transfer.products;
+
+    acc.products.push(..._products);
+    acc.transfers.push(_transfer);
+
+    return acc;
+},{transfers:[],products:[],uoms:[]})
+
+
+
+
 
 export async function generateAndSaveTransfers(){
 
@@ -8,15 +30,17 @@ export async function generateAndSaveTransfers(){
         console.error('No branches found.');
         return;
     }
-    console.log(branches);
     const _transfers=[];
+    console.log("fetching transfers...\nwait please it could take few minutes...");
     for (const [index,branch] of branches.entries()) {
 
         const{id:branch_id}=branch;
         const { transfers } =await getTransfers({ branch_id });
         _transfers.push(...transfers);
     }
-    console.log(_transfers.length);
+    console.log("...data fetched");
+    const {transfers, products, uoms} = splitTransfers({ _transfers });
+    console.log("products", products.length,"transfers", transfers.length,"uoms",uoms.length);
     
     
 }
