835e867f51bf043fc424b63c3da2c8d9dbeb86be
iwouldneverto <glebsmile3303@gmail.com>
2025-03-13 17:32:18 +0100

    new migrations

---
+++ migrations/20250313155452-createBqTransfersProductsTable.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+  const data = `CREATE TABLE bq_transfers_products (
+                                                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                       transfer_id INTEGER NOT NULL,
+                                                       FOREIGN KEY (transfer_id) REFERENCES bq_transfers(id) ON DELETE CASCADE
+                )`;
+  db.runSql(data, function (err) {
+    if (err) return console.log(err);
+    callback();
+  });
+};
+
+exports.down = function (db, callback) {
+  const data = `DROP TABLE bq_transfers_products`;
+  db.runSql(data, function (err) {
+    if (err) return console.log(err);
+    callback();
+  });
+};
+
+exports._meta = {
+  version: 1,
+};
