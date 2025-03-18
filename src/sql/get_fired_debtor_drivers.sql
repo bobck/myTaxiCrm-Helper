4e7817c9363f6caf3fcbb5192d69b0c43b528e48
iwouldneverto <glebsmile3303@gmail.com>
2025-03-18 15:55:33 +0100

    query done

--- src/sql/get_fired_debtor_drivers.sql
+++ src/sql/get_fired_debtor_drivers.sql
@@ -41,6 +41,18 @@ calculated_statements_per_c AS (
                                'b52d5c3c-9a8e-4898-8101-7c65f3ee70a4', '4ea03592-9278-4ede-adf8-f7345a856893'
                  )
      )
+,handled_cash_block_rules AS (
+    SELECT
+        fd.id AS driver_id,
+        COALESCE((SELECT (rule->>'isEnabled')::boolean FROM jsonb_array_elements(dcbr.rules) AS rule WHERE rule->>'target' = 'BALANCE'), false) AS isBalance_Enabled,
+        COALESCE((SELECT (rule->>'activationValue')::numeric FROM jsonb_array_elements(dcbr.rules) AS rule WHERE rule->>'target' = 'BALANCE'), NULL) AS balance_activation_value,
+        COALESCE((SELECT (rule->>'isEnabled')::boolean FROM jsonb_array_elements(dcbr.rules) AS rule WHERE rule->>'target' = 'DEPOSIT'), false) AS isDeposit_Enabled,
+        COALESCE((SELECT (rule->>'activationValue')::numeric FROM jsonb_array_elements(dcbr.rules) AS rule WHERE rule->>'target' = 'DEPOSIT'), NULL) AS deposit_activation_value
+    FROM driver_cash_block_rules dcbr
+             RIGHT JOIN fired_drivers fd ON dcbr.driver_id = fd.id
+    WHERE COALESCE((SELECT (rule->>'isEnabled')::boolean FROM jsonb_array_elements(dcbr.rules) AS rule WHERE rule->>'target' = 'BALANCE'), false)  = true
+       OR COALESCE((SELECT (rule->>'isEnabled')::boolean FROM jsonb_array_elements(dcbr.rules) AS rule WHERE rule->>'target' = 'DEPOSIT'), false) = true
+)
 SELECT
     fd.full_name,
     fd.auto_park_id,
@@ -49,14 +61,13 @@ SELECT
     cs_current_week.balance AS current_week_balance,
     cs_current_week.total_deposit AS current_week_total_deposit,
     cs_current_week.total_debt AS current_week_total_debt,
-    dcbr.rules as rules,
-    (fd.fire_date::date)::TEXT AS fire_date
+    (fd.fire_date::date)::TEXT AS fire_date,
+    hcbr.isBalance_Enabled,
+    hcbr.balance_activation_value,
+    hcbr.isDeposit_Enabled,
+    hcbr.deposit_activation_value
 FROM
     fired_drivers fd
-        JOIN companies c ON
-        c.id = fd.company_id
-        JOIN auto_parks ap ON
-        ap.id = fd.auto_park_id
         JOIN calculated_statements_per_c cs_fired_week ON
         cs_fired_week.year = fd.fire_year
             AND cs_fired_week.week = fd.fire_week
@@ -73,7 +84,7 @@ FROM
                 current_date
                                        )
             AND cs_current_week.driver_id = fd.id
-        LEFT JOIN driver_cash_block_rules dcbr on fd.id=dcbr.driver_id
+        LEFT JOIN handled_cash_block_rules hcbr on fd.id=hcbr.driver_id
 WHERE
     (
      cs_current_week.balance+cs_current_week.total_deposit<0
