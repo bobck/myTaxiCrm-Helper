8cc4021c4011fbf1cb84ac00540b656c853a2318
iwouldneverto <glebsmile3303@gmail.com>
2025-03-25 17:12:48 +0100

    prettied the query

--- src/sql/get-branding-cars-info.sql
+++ src/sql/get-branding-cars-info.sql
@@ -1,25 +1,29 @@
-with report_cards as
-    (select distinct on (t.car_id) t.car_id,
-                                    CASE
-                                        WHEN s.event_type != 'RENTAL' THEN s.driver_id
-                                        ELSE (s.rental_settings -> 'mapping' -> 0 ->> 'driverId')::UUID
-                                        END  AS d_id,
-                                    count(*) as total_trips
-      from trips t
-        join integrations i on t.integration_id = i.id
-          AND i.type = 'BOLT'
-          AND i.auto_park_id =any($1)
-        join schedule s on s.car_id = t.car_id
-          AND s.is_latest_version = true and is_deleted = false
-          and (s.event_type in ('BUSY_WITH_CREW', 'BUSY_WITH_PRIVATE_TRADER', 'RENTAL'))
-          and s.event_period_end > $3
-          and s.parent_id is null
-      where t.call_date >= $2
-      group by t.car_id, d_id
-      )
-select d.id as driver_id,
-        d.full_name as driver_name,
-        d.phone,
-        d.auto_park_id,
-        rc.total_trips
-       from report_cards rc join drivers d on rc.d_id =d.id
+WITH report_cards AS (
+    SELECT DISTINCT ON (t.car_id)
+        t.car_id,
+        CASE
+            WHEN s.event_type != 'RENTAL' THEN s.driver_id
+            ELSE (s.rental_settings -> 'mapping' -> 0 ->> 'driverId')::UUID
+            END AS d_id,
+        COUNT(*) AS total_trips
+    FROM trips t
+             JOIN integrations i ON t.integration_id = i.id
+        AND i.type = 'BOLT'
+        AND i.auto_park_id = ANY($1)
+             JOIN schedule s ON s.car_id = t.car_id
+        AND s.is_latest_version = TRUE
+        AND s.is_deleted = FALSE
+        AND s.event_type IN ('BUSY_WITH_CREW', 'BUSY_WITH_PRIVATE_TRADER', 'RENTAL')
+        AND s.event_period_end > $3
+        AND s.parent_id IS NULL
+    WHERE t.call_date >= $2
+    GROUP BY t.car_id, d_id
+)
+SELECT
+    d.id AS driver_id,
+    d.full_name AS driver_name,
+    d.phone,
+    d.auto_park_id,
+    rc.total_trips
+FROM report_cards rc
+         JOIN drivers d ON rc.d_id = d.id;
