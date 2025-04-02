541d2f2f1d3c3b94f0f8d24c14bcbce9b414a3c9
iwouldneverto <glebsmile3303@gmail.com>
2025-04-02 11:56:17 +0200

    few fixes

--- src/bitrix/modules/update-fired-debtor-driver-cards.mjs
+++ src/bitrix/modules/update-fired-debtor-driver-cards.mjs
@@ -39,91 +39,77 @@ function isUpdateFound({ dbcard, apicard }) {
 }
 
 async function prepareFiredDebtorDriverCSWithHandledCashBlockRules() {
-  const debtor_fired_drivers_map = new Map();
-  const today = DateTime.local().startOf('day');
-  const { weekNumber: cs_current_week, year: cs_current_year } = today;
-  console.log('getAllFiredDebtorDriver...');
-  const firedDebtorDrivers = await getAllFiredDebtorDriver();
-  const driver_ids = firedDebtorDrivers.map((driver) => driver.driver_id);
-  //fired drivers calculated statements for the last week
-  console.log('getDriverBalances...\n it could take few minutes...');
-  const { rows: actual_fired_drivers_cs } = await getDriverBalances({
-    driver_ids,
-  });
-  console.log(actual_fired_drivers_cs);
-  return;
-  //handled cash block rules only for debtors
-  console.log('getHandledCashBlockRulesInfo...');
-  const { rows: hcbr } = await getHandledCashBlockRulesInfo({
-    fired_drivers_ids: driver_ids,
-  });
-  console.log('joining...');
-  for (const [index, fd_cs] of actual_fired_drivers_cs.entries()) {
-    if (index === Number(process.env.DEBTOR_DRIVERS_CARDS_COUNT)) {
-      break;
-    }
-    const {
-      driver_id,
-      current_week_total_deposit,
-      current_week_total_debt,
-      current_week_balance,
-    } = fd_cs;
-
-    let matching_hcbr = hcbr.find((hcbr) => hcbr.driver_id === driver_id);
-    if (!matching_hcbr) {
-      matching_hcbr = {
-        driver_id,
-        is_balance_enabled: null,
-        balance_activation_value: null,
-        is_deposit_enabled: null,
-        deposit_activation_value: null,
-      };
-    }
-    const {
-      is_balance_enabled,
-      balance_activation_value,
-      is_deposit_enabled,
-      deposit_activation_value,
-    } = matching_hcbr;
-
-    debtor_fired_drivers_map.set(driver_id, {
-      current_week_total_deposit,
-      current_week_total_debt,
-      current_week_balance,
-      cs_current_week,
-      cs_current_year,
-      is_balance_enabled,
-      balance_activation_value,
-      is_deposit_enabled,
-      deposit_activation_value,
-    });
-  }
-  console.log(
-    `returning... ${debtor_fired_drivers_map.size} map`,
-    debtor_fired_drivers_map.values().map((d) => d)[0]
-  );
-  return { debtor_fired_drivers_map };
-}
-export async function updateFiredDebtorDriversCards() {
-  let debtor_fired_drivers_map;
   try {
-    const data = await prepareFiredDebtorDriverCSWithHandledCashBlockRules();
-    debtor_fired_drivers_map = data.debtor_fired_drivers_map;
+    const debtor_fired_drivers_map = new Map();
+    const today = DateTime.local().startOf('day');
+    const { weekNumber: cs_current_week, year: cs_current_year } = today;
+    const firedDebtorDrivers = await getAllFiredDebtorDriver();
+    const driver_ids = firedDebtorDrivers.map((driver) => driver.driver_id);
+    //fired drivers calculated statements for the last week
+    const { rows: actual_fired_drivers_cs } = await getDriverBalances({
+      driver_ids,
+    });
+
+    //handled cash block rules only for debtors
+    const { rows: hcbr } = await getHandledCashBlockRulesInfo({
+      fired_drivers_ids: driver_ids,
+    });
+    for (const [index, fd_cs] of actual_fired_drivers_cs.entries()) {
+      if (index === Number(process.env.DEBTOR_DRIVERS_CARDS_COUNT)) {
+        break;
+      }
+      const {
+        driver_id,
+        current_week_total_deposit,
+        current_week_total_debt,
+        current_week_balance,
+      } = fd_cs;
+      let matching_hcbr = hcbr.find((hcbr) => hcbr.driver_id === driver_id);
+      if (!matching_hcbr) {
+        matching_hcbr = {
+          driver_id,
+          is_balance_enabled: null,
+          balance_activation_value: null,
+          is_deposit_enabled: null,
+          deposit_activation_value: null,
+        };
+      }
+      const {
+        is_balance_enabled,
+        balance_activation_value,
+        is_deposit_enabled,
+        deposit_activation_value,
+      } = matching_hcbr;
+
+      debtor_fired_drivers_map.set(driver_id, {
+        current_week_total_deposit,
+        current_week_total_debt,
+        current_week_balance,
+        cs_current_week,
+        cs_current_year,
+        is_balance_enabled,
+        balance_activation_value,
+        is_deposit_enabled,
+        deposit_activation_value,
+      });
+    }
+    return { debtor_fired_drivers_map };
   } catch (e) {
     console.error({
       'function name': 'prepareFiredDebtorDriverCSWithHandledCashBlockRules',
       e,
     });
-    return;
+    return { debtor_fired_drivers_map: new Map() };
   }
+}
+export async function updateFiredDebtorDriversCards() {
+  const { debtor_fired_drivers_map } =
+    await prepareFiredDebtorDriverCSWithHandledCashBlockRules();
 
   if (debtor_fired_drivers_map.size === 0) {
     console.error('No rows found for fired debtor drivers found.');
     return;
   }
-
-  console.log(debtor_fired_drivers_map);
-  return;
 
   const processedCards = [];
   for (const [driver_id, payload] of debtor_fired_drivers_map) {
@@ -144,12 +130,6 @@ export async function updateFiredDebtorDriversCards() {
       cs_current_year,
     });
     if (!dbcard) {
-      console.log({
-        message: 'Absent card',
-        driver_id,
-        cs_current_week,
-        cs_current_year,
-      });
       continue;
     }
     if (!isUpdateFound({ dbcard, apicard: payload })) {
@@ -217,5 +197,5 @@ if (process.env.ENV === 'TEST') {
   await openSShTunnel;
   // await updateFiredDebtorDriversCards();
 
-  console.log(await prepareFiredDebtorDriverCSWithHandledCashBlockRules());
+  await prepareFiredDebtorDriverCSWithHandledCashBlockRules();
 }
