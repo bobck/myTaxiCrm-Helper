544ee088b1f62e4d75ebb45f91a4da781746cbae
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-04-15 13:22:17 +0200

    pre change commit

--- src/bq/modules/load-remonline-orders.mjs
+++ src/bq/modules/load-remonline-orders.mjs
@@ -5,7 +5,7 @@ import {
 } from '../../remonline/remonline.utils.mjs';
 import { remonlineTokenToEnv } from '../../remonline/remonline.api.mjs';
 async function prepareOrders() {
-  const { orderCount } = await getOrderCount();
+  // const { orderCount } = await getOrderCount();
   // const orderCount = 20000;
   const requestsPerCall = 5;
   const ordersPerPage = 50;
@@ -152,22 +152,6 @@ export async function loadRemonlineOrders() {
   //   handledOrderAttachments: handledOrderAttachments[0],
   // });
 
-  console.log({
-    handledOrdersWithoutAd_Campaign: handledOrders.reduce(
-      (acc, curr) => {
-        if ((
-          curr.ad_campaign &&
-          typeof curr.ad_campaign === 'object' &&
-          Object.keys(curr.ad_campaign).length > 0
-        )) {
-          acc.qty += 1;
-          acc.orderIds.push(curr.order_id);
-        }
-        return acc;
-      },
-      { qty: 0, orderIds: [] }
-    ),
-  });
 
   console.log({ reducingTime: time3 - time2 });
 }
