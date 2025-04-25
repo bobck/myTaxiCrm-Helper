dde623641ff955771ff9d913b2014960e30aec45
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-04-25 07:02:48 +0200

    prettied

--- src/remonline/remonline.utils.mjs
+++ src/remonline/remonline.utils.mjs
@@ -99,10 +99,14 @@ export async function getOrders(
 
   _orders.push(...orders);
 
-  console.log({ count, page, doneOnPrevPage, leftToFinish })
+  console.log({ count, page, doneOnPrevPage, leftToFinish });
 
   if (leftToFinish > 0) {
-    return await getOrders({ idLabels, ids, modified_at }, parseInt(page) + 1, _orders);
+    return await getOrders(
+      { idLabels, ids, modified_at },
+      parseInt(page) + 1,
+      _orders
+    );
   }
 
   return { orders: _orders };
