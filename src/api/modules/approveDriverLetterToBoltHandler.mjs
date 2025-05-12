be1d8f7f032889f4a36d53fca510fb986cdfcb5e
gsaintskad <glebsmile3303@gmail.com>
2025-05-12 13:23:50 +0200

    prettied

--- src/api/modules/approveDriverLetterToBoltHandler.mjs
+++ src/api/modules/approveDriverLetterToBoltHandler.mjs
@@ -5,7 +5,7 @@ const { OK, BAD_REQUEST } = api_status_codes;
 
 export const approveDriverLetterToBoltHandler = async (req, res) => {
   let { letter_id } = req.params;
-  letter_id=Number(letter_id)
+  letter_id = Number(letter_id);
   console.log(req.params);
   const { bitrix_deal_id } = req.query;
   if (!(letter_id === 1 || letter_id === 2)) {
@@ -25,5 +25,7 @@ export const approveDriverLetterToBoltHandler = async (req, res) => {
   }
   res
     .status(OK)
-    .json({ message: `bitrix_deal_id:${bitrix_deal_id} letter_id:${letter_id} letter has been approved` });
+    .json({
+      message: `bitrix_deal_id:${bitrix_deal_id} letter_id:${letter_id} letter has been approved`,
+    });
 };
