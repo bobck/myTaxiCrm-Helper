230d9ebf931cd0d3e03178efb511969e45f66b61
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-05-28 16:54:59 +0200

    features=>modules folder name change

--- src/api/core/core.route.mjs
+++ src/api/core/core.route.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
-import queryRouter from '../features/query/query.route.mjs';
+import queryRouter from '../modules/query/query.route.mjs';
 import * as CoreController from './core.controller.mjs';
-import referralRouter from '../features/referrals/referrals.route.mjs';
+import referralRouter from '../modules/referrals/referrals.route.mjs';
 const coreRouter = express.Router();
 
 coreRouter.use('/query', queryRouter);
