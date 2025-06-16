d8d60e320a03a02655c7cb22f29dfa08affe81b7
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-06-16 20:39:29 +0200

    - unused code

--- src/job-boards/robota.ua/modules/get_and_save_robotaua_vacancies.mjs
+++ src/job-boards/robota.ua/modules/get_and_save_robotaua_vacancies.mjs
@@ -1,7 +1,7 @@
 import { processApiResponse } from '../robotaua.business-entity.mjs';
 import { getVacancyList, getVacancyApplies } from '../robotaua.utils.mjs';
 
-const processApplies = ({ applies }) => {};
+
 
 export const getAndSaveRobotaUaVacancies = async () => {
   console.log({
@@ -9,36 +9,10 @@ export const getAndSaveRobotaUaVacancies = async () => {
     date: new Date(),
   });
   const { applies } = await getVacancyApplies();
-  // console.log(applies)
-  // const a=applies.reduce((acc,curr)=>{
-  //   if(!acc.has(curr.resumeType)){
-  //     acc.set(curr.resumeType,curr)
-  //   }
-      
-  //   return acc
-  // },new Map())
-  // console.log(a)
-  console.log(applies)
-  return
+ 
   const processedApplies = applies.map(processApiResponse);
   console.log(processedApplies);
 
-  // const example = applies.find((apply) => {
-  //   const { experiences, educations, contacts, additionalsEducations } = apply;
-  //   const { emails, phones, socials } = contacts;
-
-  //   return (
-  //     experiences.length > 0 && educations.length > 0
-  //     // &&additionalsEducations > 0
-  //   );
-  // });
-  // console.log({ example });
-  // const { experiences, educations, contacts, additionalsEducations } = example;
-  // const { emails, phones, socials } = contacts;
-  // console.log('experiences: ', experiences);
-  // console.log('educations: ', educations);
-  // console.log('contacts: ', contacts);
-  // console.log('additionalsEducations: ', additionalsEducations);
 };
 
 if (process.env.ENV === 'DEV' || process.env.ENV === 'TEST') {
