e586d33e2480ce4db8c8c7dceaf5787111838098
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-07-03 16:46:00 +0200

    CREATE TABLE drivers_ignoring_cash_block_rules

--- migrations/20250703143213-create-drivers-ignoring-cash-block-rulestable.js
+++ migrations/20250703143213-create-drivers-ignoring-cash-block-rulestable.js
@@ -5,23 +5,26 @@ var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = function (db, callback) {
+  const sql = `CREATE TABLE drivers_ignoring_cash_block_rules (
+    driver_id STRING NOT NULL, 
+    is_active BOOLEAN DEFAULT TRUE
+    )`;
+  db.runSql(sql, callback);
 };
-
-exports.down = function(db) {
-  return null;
+exports.down = function (db, callback) {
+  const sql = `DROP TABLE drivers_ignoring_cash_block_rules`;
+  db.runSql(sql, callback);
 };
-
 exports._meta = {
-  "version": 1
+  version: 1,
 };
