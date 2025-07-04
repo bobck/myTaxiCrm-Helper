262a6322b9efcc3ec5ee585dfbd6fd0f9f3e56c4
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-07-04 15:53:52 +0200

    basic file structure

--- src/api/core/core.route.mjs
+++ src/api/core/core.route.mjs
@@ -2,10 +2,12 @@ import express from 'express';
 import queryRouter from '../modules/query/query.route.mjs';
 import * as CoreController from './core.controller.mjs';
 import referralRouter from '../modules/referrals/referrals.route.mjs';
+import jobBoardRouter from '../modules/job-boards/job-board.route.mjs';
 const coreRouter = express.Router();
 
 coreRouter.use('/query', queryRouter);
 coreRouter.use('/', referralRouter);
+coreRouter.use('/job-boards', jobBoardRouter);
 
 coreRouter.use('/', CoreController.handleCore);
 
---
+++ src/api/core/middleware/core.middlewares.mjs
@@ -0,0 +1,15 @@
+import { authorizeAPIClient } from '../../api.utils.mjs';
+import { api_status_codes } from '../../api.constants.mjs';
+const { OK: SUCCESS_AUTH } = api_status_codes;
+export const authorizationMiddleware = (req, res, next) => {
+  const { api_key } = req.query;
+  const { code, status, message } = authorizeAPIClient({ api_key });
+
+  if (code !== SUCCESS_AUTH) {
+    console.log(`failed authorization under api key ${api_key}`);
+    res.status(code).json({ status, message });
+    return;
+  }
+  // console.log(`successful authorization under api key ${api_key}`);
+  next();
+};
