e45b34509bc00dc74e525244e03f52017d93e690
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-07-09 17:35:58 +0200

    .

--- migrations/20250709144042-add-dcbr-id-column-to-drcbr-table.js
+++ migrations/20250709144042-add-dcbr-id-column-to-drcbr-table.js
@@ -11,7 +11,7 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = function (db, callback) {
-  const sql = `ALTER TABLE driver_cash_block_rules ADD COLUMN driver_cash_block_rule_id TEXT NOT NULL;`;
+  const sql = `ALTER TABLE driver_cash_block_rules ADD COLUMN driver_cash_block_rule_id TEXT;`;
   db.runSql(sql, callback);
 };
 
