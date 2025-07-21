af3aa9cccd4ff524e00463e5a8569bc8ef978af4
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-07-21 10:54:26 +0200

    format

--- src/api/modules/query/query.route.mjs
+++ src/api/modules/query/query.route.mjs
@@ -3,7 +3,7 @@ import * as QueryController from './query.controller.mjs';
 import { authorizationMiddleware } from '../../core/middleware/core.middlewares.mjs';
 const queryRouter = express.Router();
 
-queryRouter.use('/',authorizationMiddleware)
+queryRouter.use('/', authorizationMiddleware);
 queryRouter.post('/', QueryController.queryHandler);
 
 export default queryRouter;
