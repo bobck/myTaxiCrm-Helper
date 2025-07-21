eb149219646e3939277986e51dd4042ade87dc6c
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-07-21 10:52:35 +0200

    auth added to /query endpoint

--- src/api/modules/query/query.route.mjs
+++ src/api/modules/query/query.route.mjs
@@ -1,7 +1,9 @@
 import express from 'express';
 import * as QueryController from './query.controller.mjs';
+import { authorizationMiddleware } from '../../core/middleware/core.middlewares.mjs';
 const queryRouter = express.Router();
 
+queryRouter.use('/',authorizationMiddleware)
 queryRouter.post('/', QueryController.queryHandler);
 
 export default queryRouter;
