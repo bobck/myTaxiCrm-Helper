72d245e2795a7cff12e9d1b66a05d6901c083742
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-09-08 12:28:09 +0200

    rule fetching init

---
+++ src/sheets/modules/sync-new-driver-auto-park-tariff-rules.mjs
@@ -0,0 +1,27 @@
+import { isUuid } from "../../shared/shared.utils.mjs";
+import { getAutoParksToSetTariffRules, getNewDriverTariffRulesByAutoParkSheetName } from "../sheets.utils.mjs";
+
+
+
+
+export const synchronizeNewDriverAutoParkTariffRules = async () => {
+
+    console.log({
+        message: 'synchronizeNewDriverAutoParkTariffRules',
+        date: new Date()
+    });
+    const autoParks = await getAutoParksToSetTariffRules();
+    const verifiedAutoParks = autoParks.filter(autoPark => isUuid(autoPark.auto_park_id));
+    console.log(verifiedAutoParks);
+
+    for (const autoPark of verifiedAutoParks) {
+        const {auto_park_id, auto_park_sheet_name}= autoPark;
+        const tariffRules= await getNewDriverTariffRulesByAutoParkSheetName(auto_park_sheet_name);
+        console.log(tariffRules);
+    }
+}
+
+
+if (process.env.ENV == 'DEV') {
+    synchronizeNewDriverAutoParkTariffRules()
+}
--- src/sheets/sheets.utils.mjs
+++ src/sheets/sheets.utils.mjs
@@ -1,7 +1,9 @@
 import { auth, sheets } from '@googleapis/sheets';
 
 const KEY_FILE_PATH = './token.json';
-const SPREADSHEET_ID = process.env.DCBR_SHEET_ID;
+const DCBR_SPREADSHEET_ID = process.env.DCBR_SHEET_ID;
+const TARIFF_SPREADSHEET_ID = process.env.TARIFF_SHEET_ID;
+
 
 // Reusable authentication and client setup
 const googleAuth = new auth.GoogleAuth({
@@ -25,7 +27,7 @@ export async function readDCBRSheetColumnA(sheetName) {
 
   try {
     const response = await client.spreadsheets.values.get({
-      spreadsheetId: SPREADSHEET_ID,
+      spreadsheetId: DCBR_SPREADSHEET_ID,
       range: range,
     });
 
@@ -40,5 +42,90 @@ export async function readDCBRSheetColumnA(sheetName) {
   } catch (err) {
     console.error(`The API returned an error for range ${range}:`, err);
     return []; // Return an empty array on error
+  }
+}
+
+
+export async function getAutoParksToSetTariffRules() {
+  try {
+    const sheetName = 'trackedCities';
+    const response = await client.spreadsheets.values.get({
+      spreadsheetId: TARIFF_SPREADSHEET_ID,
+      range: sheetName,
+    });
+
+    const rows = response.data.values;
+
+    if (!rows || rows.length <= 1) {
+      console.log(`No data found in sheet: ${sheetName}.`);
+      return [];
+    }
+
+    const dataObjects = rows.slice(1).map((row) => {
+      const [
+        auto_park_sheet_name,
+        auto_park_id
+      ] = row;
+
+      const rowObject = {
+        auto_park_id,
+        auto_park_sheet_name
+      };
+      return rowObject;
+    });
+
+    console.log(
+      `Successfully parsed ${dataObjects.length} rows from ${sheetName}.`
+    );
+    return dataObjects;
+  } catch (err) {
+    console.error(`The API returned an error for sheet ${sheetName}:`, err);
+    return [];
+  }
+}
+
+export async function getNewDriverTariffRulesByAutoParkSheetName(autoParkSheetName) {
+  try {
+    const response = await client.spreadsheets.values.get({
+      spreadsheetId: TARIFF_SPREADSHEET_ID,
+      range: `${autoParkSheetName}!A4:F`,
+    });
+
+    const rows = response.data.values;
+
+    if (!rows || rows.length <= 1) {
+      console.log(`No data found in sheet: ${sheetName}.`);
+      return [];
+    }
+
+    const dataObjects = rows.slice(1).map((row) => {
+      const [
+        percentage,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        
+      ] = row;
+
+      const rowObject = {
+        percentage,
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+      };
+      return rowObject;
+    });
+
+    console.log(
+      `Successfully parsed ${dataObjects.length} rows from ${autoParkSheetName}.`
+    );
+    return dataObjects;
+  } catch (err) {
+    console.error(`The API returned an error for sheet ${autoParkSheetName}:`, err);
+    return [];
   }
 }
