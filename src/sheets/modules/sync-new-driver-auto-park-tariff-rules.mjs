c807c53560e21d3d5ce121107abcc67269e8a1da
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-09-08 13:38:29 +0200

    save commit

--- src/sheets/modules/sync-new-driver-auto-park-tariff-rules.mjs
+++ src/sheets/modules/sync-new-driver-auto-park-tariff-rules.mjs
@@ -1,27 +1,49 @@
 import { isUuid } from '../../shared/shared.utils.mjs';
 import {
-  getAutoParksToSetTariffRules,
-  getNewDriverTariffRulesByAutoParkSheetName,
+    getAutoParksToSetTariffRules,
+    getNewDriverTariffRulesByAutoParkSheetName,
 } from '../sheets.utils.mjs';
 
 export const synchronizeNewDriverAutoParkTariffRules = async () => {
-  console.log({
-    message: 'synchronizeNewDriverAutoParkTariffRules',
-    date: new Date(),
-  });
-  const autoParks = await getAutoParksToSetTariffRules();
-  const verifiedAutoParks = autoParks.filter((autoPark) =>
-    isUuid(autoPark.auto_park_id)
-  );
-  console.log(verifiedAutoParks);
 
-  for (const autoPark of verifiedAutoParks) {
-    const { auto_park_id, auto_park_sheet_name } = autoPark;
-    const tariffRules = await getNewDriverTariffRulesByAutoParkSheetName(auto_park_sheet_name);
-    console.log(tariffRules);
-  }
+    const existingRules = []
+
+
+    const autoParks = await getAutoParksToSetTariffRules();
+    const verifiedAutoParks = autoParks.filter((autoPark) =>
+        isUuid(autoPark.auto_park_id)
+    );
+    console.log(verifiedAutoParks);
+    const autoParkWithTariffRules = new Map();
+    for (const autoPark of verifiedAutoParks) {
+        const { auto_park_id, auto_park_sheet_name } = autoPark;
+        const tariffRules = await getNewDriverTariffRulesByAutoParkSheetName(auto_park_sheet_name);
+        autoParkWithTariffRules.set(auto_park_id, tariffRules)
+        
+    }
+    console.log(autoParkWithTariffRules);
+
+
+    console.log('comparing...');
+
+
+    console.log('new rules: 1,2,3,...');
+    console.log('deleted rules: 4,5,6...');
+
+
+
+
+
+
+
+
+    console.log({
+        message: 'synchronizeNewDriverAutoParkTariffRules',
+        date: new Date(),
+    });
+
 };
 
 if (process.env.ENV == 'DEV') {
-  synchronizeNewDriverAutoParkTariffRules();
+    synchronizeNewDriverAutoParkTariffRules();
 }
