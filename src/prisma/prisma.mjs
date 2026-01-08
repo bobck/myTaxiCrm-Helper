a3d6eb0d80b2274b8c78a78e73e95aa8bd0077d0
HlibBezuhlyi <hlib.bezuhlyi9@gmail.com>
2026-01-08 16:45:17 +0100

    ci/cd fix

--- src/prisma/prisma.mjs
+++ src/prisma/prisma.mjs
@@ -6,4 +6,3 @@ const INTERNAL_PG_PORT = process.env.INTERNAL_PG_PORT;
 const getUrl = (dbName) => {
   return `postgresql://${INTERNAL_PG_USER}:${encodeURIComponent(INTERNAL_PG_PASSWORD)}@${INTERNAL_PG_HOST}:${INTERNAL_PG_PORT}/${dbName}`;
 };
-
