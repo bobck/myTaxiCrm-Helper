747f814a7d5db7806363c297223ee17f06b7dc02
HlibBezuhlyi <hlib.bezuhlyi9@gmail.com>
2026-01-26 11:14:31 +0100

    fix:removed dev mode actions

--- src/job-boards/robota.ua/modules/get_and_save_robotaua_applies.mjs
+++ src/job-boards/robota.ua/modules/get_and_save_robotaua_applies.mjs
@@ -31,8 +31,6 @@ export const getAndSaveRobotaUaVacancyApplies = async () => {
   const { applies } = await getRobotaUaVacancyApplies({
     last_apply_date,
   });
-  devLog({ applies });
-  return;
   const logInfo = {
     module: 'getAndSaveRobotaUaVacancyApplies',
     date: new Date(),
