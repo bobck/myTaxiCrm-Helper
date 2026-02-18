4770996ab07a51279bb1c5f2f4e053b37df52244
HlibBezuhlyi <hlib.bezuhlyi9@gmail.com>
2026-02-18 16:44:05 +0100

    refactor: Restructure Prisma directory, update related scripts, and add a new migration for posting scrapping tables.

---
+++ prisma/migrations/20260218154315_create_posting_scrapping_tables/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "private_api_warehouse_cells_to_posting_products" (
+    "posting_id" INTEGER NOT NULL,
+    "posting_item_id" INTEGER NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "cell_id" INTEGER NOT NULL,
+    "cell_title" TEXT,
+    "warehouse_id" INTEGER,
+    "code" TEXT,
+    "article" TEXT,
+    "title" TEXT,
+    "uom_id" INTEGER,
+    "quantity" DOUBLE PRECISION,
+    "cost" DOUBLE PRECISION,
+    "total_cost" DOUBLE PRECISION,
+    "sn_accounting" BOOLEAN,
+    "sernums_json" TEXT,
+    "warranty" INTEGER,
+    "warranty_period" INTEGER,
+    "is_custom_taxes" BOOLEAN,
+    "taxes_json" TEXT,
+    "is_enable_expiration_tracking" BOOLEAN,
+    "is_expiring_soon_alert_enabled" BOOLEAN,
+    "is_critical_alert_enabled" BOOLEAN,
+    "expiring_soon_alert_value" INTEGER,
+    "critical_alert_value" INTEGER,
+    "expiring_soon_alert_unit" INTEGER,
+    "critical_alert_unit" INTEGER,
+
+    CONSTRAINT "private_api_warehouse_cells_to_posting_products_pkey" PRIMARY KEY ("posting_id","posting_item_id")
+);
