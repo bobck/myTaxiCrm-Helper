3e4d9b6f347e66d241dba5e6dd44d5fb5ad58955
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2026-05-18 12:30:03 +0200

    perf(migrations): reduce migration count

--- prisma/migrations/20260518102908_add_branches_and_transfers/migration.sql
+++ prisma/migrations/20260518102908_add_branches_and_transfers/migration.sql
@@ -13,9 +13,9 @@ CREATE TABLE "public"."branches" (
 
 -- CreateTable
 CREATE TABLE "public"."transfers" (
-    "id" INTEGER NOT NULL,
     "branch_id" INTEGER NOT NULL,
     "warehouse_id" INTEGER NOT NULL,
+    "id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL,
     "description" TEXT,
     "created_by_id" INTEGER NOT NULL,
@@ -25,11 +25,13 @@ CREATE TABLE "public"."transfers" (
     "target_warehouse_title" TEXT NOT NULL,
     "created_by_fullname" TEXT NOT NULL,
 
-    CONSTRAINT "transfers_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "transfers_pkey" PRIMARY KEY ("branch_id","warehouse_id","id")
 );
 
 -- CreateTable
 CREATE TABLE "public"."transfer_products" (
+    "branch_id" INTEGER NOT NULL,
+    "warehouse_id" INTEGER NOT NULL,
     "transfer_id" INTEGER NOT NULL,
     "id" INTEGER NOT NULL,
     "title" TEXT NOT NULL,
@@ -41,11 +43,14 @@ CREATE TABLE "public"."transfer_products" (
     "uom_title" TEXT,
     "uom_description" TEXT,
 
-    CONSTRAINT "transfer_products_pkey" PRIMARY KEY ("transfer_id","id")
+    CONSTRAINT "transfer_products_pkey" PRIMARY KEY ("branch_id","warehouse_id","transfer_id","id")
 );
 
 -- CreateIndex
 CREATE INDEX "transfers_created_at_idx" ON "public"."transfers"("created_at");
 
 -- CreateIndex
-CREATE INDEX "transfers_branch_id_idx" ON "public"."transfers"("branch_id");
+CREATE INDEX "transfers_id_idx" ON "public"."transfers"("id");
+
+-- CreateIndex
+CREATE INDEX "transfer_products_transfer_id_idx" ON "public"."transfer_products"("transfer_id");
