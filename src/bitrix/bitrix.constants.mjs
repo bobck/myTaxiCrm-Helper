415c9e49b4de4673439f9fe9241d7e5e2b48ab21
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2026-08-25 12:58:46 +0200

    Revert "fix: update referral payment assignees"
    This reverts commit b039ad9b1b1d4f3c6bc9f821bfb262ab9e866b80.

--- src/bitrix/bitrix.constants.mjs
+++ src/bitrix/bitrix.constants.mjs
@@ -313,7 +313,7 @@ export const cityTextRecruitToPayments = {
   },
   Харків: {
     id: '2882',
-    assigned_by_id: '231992',
+    assigned_by_id: '166486',
   },
   Одеса: {
     id: '2884',
@@ -393,7 +393,7 @@ export const cityTextRecruitToPayments = {
   },
   Мукачево: {
     id: '2938',
-    assigned_by_id: '232094',
+    assigned_by_id: '173554',
   },
   "Кам'янське": {
     id: '2924',
